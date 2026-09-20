3256adff114d9f049650d7370c0e71bac856c005
김건우 <seedew08@gmail.com>
2026-09-20 19:13:26 +0900

    7

--- src/app/firebase/config.ts
+++ src/app/firebase/config.ts
@@ -6,10 +6,8 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
-const api_Key = process.env.FIREBASE_API_KEY ?? "";
-const firebase_domain = process.env.FIREBASE_DOMAIN ?? "";
-console.log(api_Key);
-console.log(firebase_domain);
+const api_Key = import.meta.env.VITE_FIREBASE_API_KEY;
+const firebase_domain = import.meta.env.VITE_FIREBASE_DOMAIN;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
