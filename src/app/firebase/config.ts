237cd30db1dfb1de8554b0aba6c381aa433d53ce
김건우 <seedew08@gmail.com>
2026-09-20 19:08:43 +0900

    5

--- src/app/firebase/config.ts
+++ src/app/firebase/config.ts
@@ -8,6 +8,8 @@ import { getFirestore } from "firebase/firestore";
 
 const api_Key = process.env.FIREBASE_API_KEY ?? "";
 const firebase_domain = process.env.FIREBASE_DOMAIN ?? "";
+console.log(api_key);
+console.log(firebase_domain);
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
